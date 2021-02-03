ecd03aa8cf80aabfc9bda10cd8b538e2e6f6f301
Mendim Arifaj <mendim@gjirafa.com>
2021-02-03 14:30:43 +0100

    store changed

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,42 @@
+/* eslintrc-disable */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    sampleData: '1323',
+    name: 'mendim',
+    apiData: {}
   },
   mutations: {
+    changeName(state, name) {
+      state.name = name;
+    },
+    GET_DATA(state, data){
+      state.apiData = data;
+    }
   },
   actions: {
+    changeName({ commit }) {
+      commit('changeName', 'mendmania');
+    },
+
+    async apiData({ commit }) {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        if (response.status !== 200) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+          commit('GET_DATA', response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   modules: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{ meni }}</h2>
+    <h2>{{ sampleData }}</h2>
+    <h2>{{ displayName }}</h2>
+    <button @click="changeValue">click</button>
+
+    <h1>{{componentData}}</h1>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      meni: '2',
+      componentData: "no data"
+    };
+  },
+  computed: {
+    sampleData() {
+      return this.$store.state.sampleData;
+    },
+    displayName() {
+      return this.$store.state.name;
+    },
+    displayApiData(){
+      return this.$store.state.apiData
+    }
+  },
+  methods: {
+    changeValue  () {
+      this.meni = '2213';
+      this.$store.dispatch('changeName');
+      const res = this.$store.dispatch("apiData");
+      console.log(res);
+      this.componentData = this.$store.state.apiData
+    },
+  },
+  beforeCreate() { console.log('beforeCreate'); },
+  created() { console.log('created');
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then((response) => {
+      this.meni = response.data;
+    }); },
+  beforeMount() { console.log('beforeMount'); },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() { console.log('beforeUpdate'); },
+  updated() { console.log('updated'); },
+  beforeDestroy() { console.log('beforeDestroy'); },
+  destroyed() { console.log('destroyed'); },
+
+};
+</script>
+
+<style scoped>
+.about {
+  background: black;
+}
+</style>
